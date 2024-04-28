7c20352a866dc1aad19f8601b6beb88b512fe347
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-04-28 14:44:30 +0300

    Add files via upload

---
+++ CSharpEdition/WinRegistry/WinReg.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Threading;
+namespace WinRegistry
+{
+    public class WinReg
+    {
+        public void WriteInLocalMachineDWORD(string subkey, string str, object object_any)
+        {
+            RegistryKey reg = Registry.LocalMachine.OpenSubKey(subkey, true);
+            if(reg == null)
+            {
+                Debug.WriteLine("Failed To Open Sub Key!!!");
+            }
+            reg.SetValue(str, object_any, RegistryValueKind.DWord);
+        }
+    }
+}
