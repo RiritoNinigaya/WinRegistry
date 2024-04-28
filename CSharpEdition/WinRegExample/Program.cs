7118563cf6a57af4a1614e196ae037e10945552f
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-04-28 21:39:48 +0300

    Added WinRegistry Example
    Added WinRegistry Example!!!

---
+++ CSharpEdition/WinRegExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinRegistry;
+
+namespace WinRegExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WinReg registr = new WinReg();
+            registr.WriteStrValue(Microsoft.Win32.RegistryHive.LocalMachine, "SOFTWARE\\Policies\\Microsoft\\Windows\\System", "Test1", "Test1");
+            Environment.Exit(1112);
+        }
+    }
+}
